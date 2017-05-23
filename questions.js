155013b0ff67d81b48cff91c32a69a649eb18b3a
robinBanquo <caramaschi@hotmail.com>
2017-05-23 18:22:11 +0200

    ou qu'on en est

---
+++ questions.js
@@ -0,0 +1,34 @@
+var questions = [
+                  {
+                    "question" : "Quel est le nom de famille de Freddy dans la série des films éprouvantes « Freddy, les griffes de la nuit,... »?" ,
+                    "r0" : "Krueger",
+                    "r1" : "Grouger",
+                    "r2" : "Kuster",
+                    "r3" : "ta mère",
+                    "goodone" : "r0"
+                  },
+                  {
+                    "question" : "Quels mots Robert Mitchum a-t-il de tatoués sur ses mains dans « La nuit du chasseur »?" ,
+                    "r0" : "life and dead",
+                    "r1" : "Love et hate",
+                    "r2" : "More et Less",
+                    "r3" : "ta mère",
+                    "goodone" : "r1"
+                  },
+                  {
+                    "question" : "Dans quelle ville officie le très cartoonesque The Mask ?" ,
+                    "r0" : "Central City",
+                    "r1" : "Edge City",
+                    "r2" : "Gotham City",
+                    "r3" : "Carrouf city",
+                    "goodone" : "r1"
+                  },
+                  {
+                    "question" : "Dans Source Code, combien de temps en arrière peut revenir Colter Stevens ?" ,
+                    "r0" : "6 minutes",
+                    "r1" : "8 minutes",
+                    "r2" : "10 minutes",
+                    "r3" : "12 minutes",
+                    "goodone" : "r1"
+                  }
+                ];
